a175ae92875df86ff5d55600e62307d69c2254cf
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-02-19 18:52:21 +0100

    updated

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ dotenv.config();
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename)
+
+const __dirname = BudgetApp
 
 {/*
 const con = mysql.createConnection({
