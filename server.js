86a637e4d779dac4a44ff436279ccab5eadec305
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-03-16 10:13:30 +0100

    updated

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const con = mysql.createConnection({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.PORT
+    port: process.env.DBPORT
 })
 
 con.connect(err => {
